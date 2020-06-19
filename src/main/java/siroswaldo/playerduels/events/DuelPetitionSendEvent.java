3090a0b4a184bbb03f65e6db4bf626d8e8167b0d
SirOswaldo <DeveloperOswaldo@gmail.com>
2020-06-19 18:57:35 -0300

    DuelPetitionSendEvent added!

---
+++ src/main/java/siroswaldo/playerduels/events/DuelPetitionSendEvent.java
@@ -0,0 +1,52 @@
+package siroswaldo.playerduels.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class DuelPetitionSendEvent extends Event implements Cancellable {
+
+    public static HandlerList handlerList = new HandlerList();
+
+    private boolean cancel;
+    private final Player challenger;
+    private final Player challenged;
+
+    public DuelPetitionSendEvent(Player challenger, Player challenged) {
+        this.challenger = challenger;
+        this.challenged = challenged;
+    }
+
+    public Player getChallenger() {
+        return challenger;
+    }
+
+    public Player getChallenged() {
+        return challenged;
+    }
+
+    @Override
+    public String getEventName() {
+        return "DuelPetitionSendEvent";
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+}
