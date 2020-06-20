3a8b1d0d277f0d67cdda0dd499d6c16187d3bcba
SirOswaldo <DeveloperOswaldo@gmail.com>
2020-06-19 23:31:11 -0300

    Arena.java update!

---
+++ src/main/java/siroswaldo/playerduels/arena/Arena.java
@@ -0,0 +1,82 @@
+package siroswaldo.playerduels.arena;
+
+import org.bukkit.Location;
+import siroswaldo.playerduels.duel.Duel;
+
+public class Arena {
+
+    private final String name;
+    private boolean enable;
+
+    private Location hubLocation;
+    private Location challengerLocation;
+    private Location challengedLocation;
+
+    private String status;
+    private Duel duel;
+
+    public Arena(String name, Location hubLocation, Location challengerLocation, Location challengedLocation) {
+        this.name = name;
+        this.hubLocation = hubLocation;
+        this.challengerLocation = challengerLocation;
+        this.challengedLocation = challengedLocation;
+        enable = (challengerLocation != null) && (challengedLocation != null);
+        if (enable){
+            status = "WAITING";
+        } else {
+            status = "NEED_EDIT";
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getHubLocation() {
+        return hubLocation;
+    }
+
+    public void setHubLocation(Location hubLocation) {
+        this.hubLocation = hubLocation;
+    }
+
+    public Location getChallengerLocation() {
+        return challengerLocation;
+    }
+
+    public void setChallengerLocation(Location challengerLocation) {
+        this.challengerLocation = challengerLocation;
+    }
+
+    public Location getChallengedLocation() {
+        return challengedLocation;
+    }
+
+    public void setChallengedLocation(Location challengedLocation) {
+        this.challengedLocation = challengedLocation;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public Duel getDuel() {
+        return duel;
+    }
+
+    public void setDuel(Duel duel) {
+        this.duel = duel;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
